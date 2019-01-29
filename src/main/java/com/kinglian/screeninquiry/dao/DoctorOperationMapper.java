ca88051cfcb5df77a9c33deb0f95199f50b0f4c7
dell-PC\dell <61469049@qq.com>
2019-01-29 21:18:30 +0800

    大屏机接口

--- src/main/java/com/kinglian/screeninquiry/dao/DoctorOperationMapper.java
+++ src/main/java/com/kinglian/screeninquiry/dao/DoctorOperationMapper.java
@@ -40,7 +40,7 @@ public interface DoctorOperationMapper {
             "WHERE\n" +
             "\tmov.cdid = #{doctorId} \n" +
             "\tAND mov.visit_status = '0' \n" +
-            "\t ")
+            "\t order by created_date desc")
     List<DoctorPendingOrderRep> selectPengdingOrder(@Param("doctorId") String doctorId);
 
     /**
