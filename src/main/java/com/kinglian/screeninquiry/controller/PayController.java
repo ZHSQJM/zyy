7c7a9469a42f94348bd0cc0535b91403d9c1dbb5
ZHSYQ <2534798858@qq.com>
2019-04-16 10:09:34 +0800

    添加微信支付接口

--- src/main/java/com/kinglian/screeninquiry/controller/PayController.java
+++ src/main/java/com/kinglian/screeninquiry/controller/PayController.java
@@ -1,93 +1,155 @@
 package com.kinglian.screeninquiry.controller;
 
-import com.kinglian.screeninquiry.WeixinPay.WxparaInfo;
-import com.kinglian.screeninquiry.WeixinPay.WxpayInfo;
-import com.kinglian.screeninquiry.WeixinPay.WxpayUtil;
-import com.kinglian.screeninquiry.config.WxpayConfig;
+import com.kinglian.screeninquiry.constant.Constant;
 import com.kinglian.screeninquiry.model.Vo.PayWeiXinVo;
 import com.kinglian.screeninquiry.utils.R;
+import com.kinglian.screeninquiry.weixin.*;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 import java.util.Map;
 
-/**
- * @project: zblog
- * @author: zhs
- * @date: 2019/4/15 10:03
- * @package: com.kinglian.screeninquiry.controller
- * @description:
- */
 @Controller
-@RequestMapping("api")
 @Slf4j
 public class PayController {
 
-    /**
-     * 统一下单  直接传参
-     * @param orderid
-     * @param openid
-     * @param fee
-     * @param wxparaInfo
-     * @return
-     */
+
     @ResponseBody
     @RequestMapping(value = "/prepay", produces = "text/html;charset=UTF-8")
-    public R<PayWeiXinVo> unifiedOrderByPara(String orderid, String openid, String fee, WxparaInfo wxparaInfo){
+    public R<PayWeiXinVo> prePay(String openId, ModelMap model, HttpServletRequest request) {
         PayWeiXinVo payWeiXinVo = new PayWeiXinVo();
-        // 封装统一下单基本参数
-        WxpayInfo wxinfo = new WxpayInfo();
-        wxinfo.setAppid(wxparaInfo.getSaid());
-        wxinfo.setMch_id(wxparaInfo.getSmid());
 
-        wxinfo.setSub_appid(wxparaInfo.getSsaid());
-        wxinfo.setSub_mch_id(wxparaInfo.getSsmid());
+            openId = openId.replace("\"", "").trim();
 
-        wxinfo.setAttach(wxparaInfo.getSath());
-        wxinfo.setBody(wxparaInfo.getSbody());
+            String clientIP = CommonUtil.getClientIp(request);
 
-        wxinfo.setNonce_str(WxpayUtil.create_nonce_str());
-        wxinfo.setNotify_url(wxparaInfo.getSnou());
+            log.error("openId: " + openId + ", clientIP: " + clientIP);
+            //获取随机数
+            String randomNonceStr = RandomUtils.generateMixString(32);
+            String prepayId = unifiedOrder(openId, clientIP, randomNonceStr);
 
-        wxinfo.setOut_trade_no(orderid);
-        //.multiply(new BigDecimal(100))
-        wxinfo.setTotal_fee(fee);
-        wxinfo.setTrade_type(wxparaInfo.getStdp());
-        wxinfo.setSpbill_create_ip(wxparaInfo.getSip());
-        wxinfo.setSub_openid(openid);
-        wxinfo.setDevice_info(wxparaInfo.getSdvi());
-        System.out.println("签名前参数："+wxinfo.toString());
-        String sign = WxpayUtil.getSign(wxinfo,wxparaInfo.getSkey());
-        wxinfo.setSign(sign);
+            log.error("prepayId: " + prepayId);
 
-        Map<String, String> map = WxpayUtil.httpsRequestToXML(WxpayConfig.unifiedOrderUrl,WxpayConfig.method,WxpayUtil.payInfoToXML(wxinfo));
-        System.out.println("统一下单结果："+map.toString());
-        String preid = map.get("prepay_id");
-        System.out.println("预支付id结果："+preid);
-        Map<String, Object> result = new HashMap<String, Object>();
+            if(StringUtils.isBlank(prepayId)) {
+            } else {
+                payWeiXinVo.setPackage1(prepayId).setNonceStr(randomNonceStr);
 
-        // 返回结果（请求支付参数）进行封装
-        if(map.get("return_code") != null && map.get("return_code").equals("SUCCESS")) {
-            long times = System.currentTimeMillis() / 1000;
-
-            payWeiXinVo.setNonceStr(map.get("nonce_str")).setPackage1(map.get("prepay_id")).setTimeStamp(String.valueOf(times));
-
-          /*  result.put("appId", map.get("appid"));
-            result.put("nonceStr", map.get("nonce_str"));
-            result.put("package", "prepay_id="+map.get("prepay_id"));
-            result.put("timeStamp", String.valueOf(times));
-            result.put("orderid",orderid);
-            map.put("times", String.valueOf(times));*/
-            String signs = WxpayUtil.getSingWxPayApp(map,wxparaInfo.getSkey());
-            payWeiXinVo.setPaySign(signs);
-        }else{
-            System.out.println("统一下单失败！！！");
-        }
-
-        return new R<PayWeiXinVo>(payWeiXinVo);
+            }
+        return new  R<PayWeiXinVo>(payWeiXinVo);
     }
 
+
+    /**
+     * 调用统一下单接口
+     * @param openId
+     */
+    private String unifiedOrder(String openId, String clientIP, String randomNonceStr) {
+
+        try {
+
+            String url = Constant.URL_UNIFIED_ORDER;
+
+            PayInfo payInfo = createPayInfo(openId, clientIP, randomNonceStr);
+            String md5 = getSign(payInfo);
+            payInfo.setSign(md5);
+
+            log.error("md5 value: " + md5);
+
+            String xml = CommonUtil.payInfoToXML(payInfo);
+            xml = xml.replace("__", "_").replace("<![CDATA[1]]>", "1");
+            //xml = xml.replace("__", "_").replace("<![CDATA[", "").replace("]]>", "");
+            log.error(xml);
+
+            StringBuffer buffer = HttpUtil.httpsRequest(url, "POST", xml);
+            log.error("unifiedOrder request return body: \n" + buffer.toString());
+            Map<String, String> result = CommonUtil.parseXml(buffer.toString());
+
+
+            String return_code = result.get("return_code");
+            if(StringUtils.isNotBlank(return_code) && return_code.equals("SUCCESS")) {
+
+                String return_msg = result.get("return_msg");
+                if(StringUtils.isNotBlank(return_msg) && !return_msg.equals("OK")) {
+                    //log.error("统一下单错误！");
+                    return "";
+                }
+
+                String prepay_Id = result.get("prepay_id");
+                return prepay_Id;
+
+            } else {
+                return "";
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    private PayInfo createPayInfo(String openId, String clientIP, String randomNonceStr) {
+
+        Date date = new Date();
+        String timeStart = TimeUtils.getFormatTime(date, Constant.TIME_FORMAT);
+        String timeExpire = TimeUtils.getFormatTime(TimeUtils.addDay(date, Constant.TIME_EXPIRE), Constant.TIME_FORMAT);
+
+        String randomOrderId = CommonUtil.getRandomOrderId();
+
+        PayInfo payInfo = new PayInfo();
+        payInfo.setAppid(Constant.APP_ID);
+        payInfo.setMch_id(Constant.MCH_ID);
+        payInfo.setDevice_info("WEB");
+        payInfo.setNonce_str(randomNonceStr);
+        payInfo.setSign_type("MD5");
+        payInfo.setBody("JSAPI支付测试");
+        payInfo.setAttach("支付测试4luluteam");
+        payInfo.setOut_trade_no(randomOrderId);
+        payInfo.setTotal_fee(1);
+        payInfo.setSpbill_create_ip(clientIP);
+        payInfo.setTime_start(timeStart);
+        payInfo.setTime_expire(timeExpire);
+        payInfo.setNotify_url(Constant.URL_NOTIFY);
+        payInfo.setTrade_type("JSAPI");
+        payInfo.setLimit_pay("no_credit");
+        payInfo.setOpenid(openId);
+
+        return payInfo;
+    }
+
+    private String getSign(PayInfo payInfo) throws Exception {
+        StringBuffer sb = new StringBuffer();
+        sb.append("appid=" + payInfo.getAppid())
+                .append("&attach=" + payInfo.getAttach())
+                .append("&body=" + payInfo.getBody())
+                .append("&device_info=" + payInfo.getDevice_info())
+                .append("&limit_pay=" + payInfo.getLimit_pay())
+                .append("&mch_id=" + payInfo.getMch_id())
+                .append("&nonce_str=" + payInfo.getNonce_str())
+                .append("&notify_url=" + payInfo.getNotify_url())
+                .append("&openid=" + payInfo.getOpenid())
+                .append("&out_trade_no=" + payInfo.getOut_trade_no())
+                .append("&sign_type=" + payInfo.getSign_type())
+                .append("&spbill_create_ip=" + payInfo.getSpbill_create_ip())
+                .append("&time_expire=" + payInfo.getTime_expire())
+                .append("&time_start=" + payInfo.getTime_start())
+                .append("&total_fee=" + payInfo.getTotal_fee())
+                .append("&trade_type=" + payInfo.getTrade_type())
+                .append("&key=" + Constant.APP_KEY);
+
+        log.error("排序后的拼接参数：" + sb.toString());
+
+        return CommonUtil.getMD5(sb.toString().trim()).toUpperCase();
+    }
+
+
+
 }
+
+
