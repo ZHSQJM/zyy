ab89b3d0f443ab035bd4c2412a28f09a537e2214
ZHSYQ <2534798858@qq.com>
2019-04-16 11:14:13 +0800

    添加微信支付接口

---
+++ src/main/java/com/kinglian/screeninquiry/WeixinPay/WxpayConfig.java
@@ -0,0 +1,15 @@
+package com.kinglian.screeninquiry.WeixinPay;
+
+/**
+ * @project: zblog
+ * @author: zhs
+ * @date: 2019/4/15 10:55
+ * @package: com.kinglian.screeninquiry.config
+ * @description:
+ */
+public class WxpayConfig {
+
+    public static String unifiedOrderUrl = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+    public static String method = "POST";
+
+}
